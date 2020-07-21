8ed246c02bc4f59bf7b76006a9ba303ee4f0f889
Croydon <git@cr0ydon.com>
2020-07-21 12:15:21 +0200

    Replace social media URLs too

--- replace-urls.py
+++ replace-urls.py
@@ -17,6 +17,10 @@ for subdir, dirs, files in os.walk("./"):
                     # Special case handling
                     # On the home page the home page link contains just index.html
                     line = line.replace("href=\"index.html\"", "href=\"/\"")
+                    line = line.replace(
+                        "content=\"http://localhost/", 
+                        "content=\"https://blog.cr0ydon.com/"
+                        )
                     
                     content += re.sub("href=\"(?!http)(.*)/index\.html\"",
                                     "href=\"\\1/\"", 
