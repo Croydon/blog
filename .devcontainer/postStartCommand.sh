49efec8d50bb6ad5ec7ebb6157c32fc3d4ad92b0
Croydon <git@cr0ydon.com>
2022-11-12 07:24:06 +0100

    Fix CI/devcontainer

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -3,6 +3,7 @@ set -e
 
 composer --working-dir=/var/www/html --no-cache update
 
+mkdir -p /var/www/html/assets
 curl -L https://github.com/Croydon/blog-assets/archive/refs/heads/main.tar.gz | tar -xz --strip-components=1 -C /var/www/html/assets
 
 service apache2 start
